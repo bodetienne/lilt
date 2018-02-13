e1b96fc08dfc8d8c65c7698c83beac90fbfda457
Emma Herve <emma.herve@my-digital-school.org>
2018-02-13 15:30:55 +0100

    Composition options

--- composition.php
+++ composition.php
@@ -9,12 +9,15 @@
 	<link rel="stylesheet" href="vendors/Bootstrap/vendors/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" href="style/style_home2.css">
 	<link rel="stylesheet" href="style/style2.css">
+	<script src="js/emmajs.js"></script>
 	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+
 </head>
 
 <body>
-	
+
+
 					
 
 <!----------------------------------------------------
@@ -31,12 +34,15 @@
 	<div class="page_compo">
 		<div class="lecteur_compo">
 			<ul>
+				
+				<li>
+				<div class="innerCompo">
 			
-				<li><img src="Images/Icon/playbutton.png" alt="bouton play"></li>
+					<img src="Images/Icon/playbutton.png" alt="bouton play">
 			
 			
 			
-				<li>Can I stay | <?php
+					<p> Can I stay | <?php
 	
 								if($_SESSION['nom_utilisateur'] !== ""){
 								$user = $_SESSION['nom_utilisateur'];
@@ -44,28 +50,86 @@
 								echo "$user";
 								}
            					?>  
-				</li>
+					</p>
+				
 			
 
 					<script src="vendors/audiojs/audio.min.js"></script>
 					<script>  audiojs.events.ready(function() {var as = audiojs.createAll();});</script>
 				
 				
-				<li>
-					<audio src="lecteur/music/Bruno Mars - When I Was Your Man.mp3" preload="auto" />
-				</li>
+					<div class="CompoMedia">
+						<audio src="lecteur/music/Bruno Mars - When I Was Your Man.mp3" preload="auto" />
+						<img src="Images/Icon/fast-forward2.png" alt="precedent"><img src="Images/Icon/fast-forward1.png" alt="suivant">
+					</div>	
+						
+				
 			
 			
 			
-				<li>
+<!--			 	<li>
 					<img src="Images/Icon/fast-forward2.png" alt="precedent"><img src="Images/Icon/fast-forward1.png" alt="suivant">
-				</li>
+				</li>-->
 			
 			
-				<li>
+			
 					<img src="Images/Icon/update-arrow.png" alt="replaySong">
 					<img src="Images/Icon/shuffle.png" alt="aléatoire">
-					<img src="Images/Icon/mode-circular-button.png" alt="optionSong">
+					<input id="optionSong" type=image src="Images/Icon/mode-circular-button.png" alt="optionSong" width="40px"><!-- Permet d'ouvrir le menu-->
+					
+					<!-- Le menu -->
+					<div id="myModal" class="modal">
+
+						<!-- Contenu Menu -->
+						<div class="modal-content">
+							
+							<div class="modal-header">
+								<span class="close">&times;</span>
+								<h2>Options</h2>
+							</div>
+							<div class="modal-body">
+							  <p><img src="Images/Icon/coeurVide.png">Ajouter aux favoris</p>
+							  <p>Ajouter à la playlist</p>
+							</div>
+							<div class="modal-footer">
+							  <h3></h3>
+							</div>
+							
+						</div>
+
+					</div>
+			
+					
+					<script>
+						// ------- Menu Option Composition -------- //
+
+						// Get the modal
+						var modal = document.getElementById('myModal');
+
+						// Get the button that opens the modal
+						var btn = document.getElementById("optionSong");
+
+						// Get the <span> element that closes the modal
+						var span = document.getElementsByClassName("close")[0];
+
+						// When the user clicks the button, open the modal 
+						btn.onclick = function() {
+							modal.style.display = "block";
+						}
+
+						// When the user clicks on <span> (x), close the modal
+						span.onclick = function() {
+							modal.style.display = "none";
+						}
+
+						// When the user clicks anywhere outside of the modal, close it
+						window.onclick = function(event) {
+							if (event.target == modal) {
+								modal.style.display = "none";
+							}
+						}
+					</script>
+				</div>
 				</li>
 				
 			</ul>
