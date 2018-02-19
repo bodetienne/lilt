0a022900f649d9abe2d96e81716a9b2994025674
Etienne Bodet <salut@bonjour.com>
2018-02-19 16:46:16 +0100

    modif home

--- lecteur.php
+++ lecteur.php
@@ -35,152 +35,37 @@
 			
 <?php 
 
-	
+	require 'PDO/includes/pdo.php';		
 
 	echo'
 	<div class="grand-container">
 		<h2 class="title">' . $_POST["tag"] . '</h2>';
 		echo '<div class="inner">
 			<div class="list">
-				<ul id="playlist">';
-					
-		
-
-					
-					
-					//valeur des différentes playlists dans les variables pour éviter les fautes de frappes dans l'algorithme
-					/*$rock = "Rock";
-					$rap = "Rap";
-					$soul = "Soul";
-					$pop = "Pop";
-					$reggae = "Reggae";
-					$notag = "No tag"; 
-					
-						if ($_POST["tag"] == $rock ) { //si rock choisi dans le formulaire --> entrer dans la boucle 
-					
-							$query_select = "SELECT * FROM chanson WHERE tag='" . $rock . "'"; //requette mysql : selection dans la base tab chanson (contenue dans * : tout), ou le tag = rock 
-
-							if ($stmt = mysqli_prepare($link, $query_select)){ //variable = fonction de preparation de la requette avant l'éxécution
-								mysqli_stmt_execute($stmt); //exécution de la fonction
-
-								mysqli_stmt_bind_result($stmt,$idChanson, $nomChanson, $dureeChanson, $tag, $mp3); //bind = lier, on cible les colonnes de la tab en question 
-
-								echo('<div class"succes">');
-
-								while(mysqli_stmt_fetch($stmt)){ //renvoie les résultats de la requette
-									echo('<p>Song name = ' . $nomChanson . ', duration = ' . $dureeChanson . ' </p>');
-								}
-							}
-
-							//répétition de la boucle pour tous les tags
-						} else if ($_POST["tag"] == $rap ) {
-					
-							$query_select = "SELECT * FROM chanson WHERE tag='" . $rap . "'";
-
-							if ($stmt = mysqli_prepare($link, $query_select)){
-								mysqli_stmt_execute($stmt);
-
-								mysqli_stmt_bind_result($stmt,$idChanson, $nomChanson, $dureeChanson, $tag, $mp3);
-
-								echo('<div class"succes">');
-
-								while(mysqli_stmt_fetch($stmt)){
-									echo('<p>Song name = ' . $nomChanson . ', duration = ' . $dureeChanson . '</p>');
-								}
-							} 
-
-							
-						} else if ($_POST["tag"] == $soul ) {
-					
-							$query_select = "SELECT * FROM chanson WHERE tag='" . $soul . "'";
-
-							if ($stmt = mysqli_prepare($link, $query_select)){
-								mysqli_stmt_execute($stmt);
-
-								mysqli_stmt_bind_result($stmt,$idChanson, $nomChanson, $dureeChanson, $tag, $mp3);
-
-								echo('<div class"succes">');
-
-								while(mysqli_stmt_fetch($stmt)){
-									echo('<p>Song name = ' . $nomChanson . ', duration = ' . $dureeChanson . '</p>');
-								}
-							} 
-
-							
-						} else if ($_POST["tag"] == $pop ) {
-					
-							$query_select = "SELECT * FROM chanson WHERE tag='" . $pop . "'";
-
-							if ($stmt = mysqli_prepare($link, $query_select)){
-								mysqli_stmt_execute($stmt);
-
-								mysqli_stmt_bind_result($stmt,$idChanson, $nomChanson, $dureeChanson, $tag, $mp3);
-
-								echo('<div class"succes">');
-
-								while(mysqli_stmt_fetch($stmt)){
-									echo('<p>Song name = ' . $nomChanson . ', duration = ' . $dureeChanson . '</p>');
-								}
-							} 
-
-
-							
-						} else if ($_POST["tag"] == $reggae ) {
-					
-							$query_select = "SELECT * FROM chanson WHERE tag='" . $reggae . "'";
-
-							if ($stmt = mysqli_prepare($link, $query_select)){
-								mysqli_stmt_execute($stmt);
-
-								mysqli_stmt_bind_result($stmt,$idChanson, $nomChanson, $dureeChanson, $tag, $mp3);
-
-								echo('<div class"succes">');
-
-								while(mysqli_stmt_fetch($stmt)){
-									echo('<p>Song name = ' . $nomChanson . ', duration = ' . $dureeChanson . ' <audio controls="">
-												<source src="'. $mp3 .'" type="audio/mpeg"/>
-											</audio> </p>');
-								}
-							 }  
-							} else if ($_POST["tag"] == $notag ) {
-					
-							$query_select = "SELECT * FROM chanson WHERE tag='" . $notag . "'";
-
-							if ($stmt = mysqli_prepare($link, $query_select)){
-								mysqli_stmt_execute($stmt);
-
-								mysqli_stmt_bind_result($stmt,$idChanson, $nomChanson, $dureeChanson, $tag, $mp3);
-
-								echo('<div class"succes">');
-
-								while(mysqli_stmt_fetch($stmt)){
-									echo('<p>Song name = ' . $nomChanson . ', duration = ' . $dureeChanson . '</p>');
-								}
-							 }
-							} */
+				<ul id="playlist">'; 	
 						
-					require 'PDO/includes/pdo.php';
 					
 						$tag = $_POST["tag"];
 					
 					try {
+						$recupId = "SELECT `idUtilisateur` FROM `utilisateur` WHERE `nom_utilisateur`= '$user'";
+						$stmt = $connexion -> prepare($recupId);
+						$stmt -> execute();
+						
 						$query_select = "SELECT * FROM chanson WHERE tag=:tag";
 						$stmt = $connexion->prepare($query_select);
 						$stmt -> bindValue(':tag', $tag); //bind = lier
 						$stmt -> execute();
 					
-						/*$query_artiste = "SELECT * FROM artite WHERE tag=:id";
-						$stmt = $connexion->prepare($query_select);
-						$stmt -> bindValue(':id', $idUtilisateur); //bind = lier
-						$stmt -> execute();*/
+						$query_artiste = "SELECT * FROM artiste WHERE idUtilisateur= '$recupId'";
+						$stmt = $connexion -> prepare($query_artiste);
+						$stmt -> execute();
 					
 						while($chanson = $stmt -> fetch()) {
 						echo "<div class='lecteur'> <p>" . $chanson -> nomChanson . "</p>";
-							/*while($artiste = $stmt -> fetch()) {
-								
-							}*/
-
-
+							while($artiste = $stmt -> fetch()) {
+								echo "<p>". $artiste -> nomArtiste ."</p>";
+							}
 						echo "</div>";
 						}
 					
@@ -190,13 +75,7 @@
 						echo "<pre>";
 						var_dump ($e -> getTrace());
 						echo "</pre>";
-					}
-
-					
-					
-							 
-
-
+					}				
 							
 				echo '</ul>
 			</div>
